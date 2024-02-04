bb811553de2bcd3cab0b8ce9401bab816098c227
fialaivo <fiala.ivos@gmail.com>
2024-02-04 22:28:59 +0100

    přidána funkce check_duplication - kontrola duplicit

--- projekt_2.py
+++ projekt_2.py
@@ -21,6 +21,14 @@ def create_random_number():
     digits.append("0")
   return(random_number)
 
+def check_duplication(user_number):
+  duplication = False
+  for cycle_number in range(0,3):
+    if user_number[cycle_number] in user_number[cycle_number+1]:
+      duplication = True
+      break
+  return duplication
+
 start_time = time.time()
 line = "-" * 40
 user_number = 0
@@ -32,6 +40,7 @@ print("I've generated a random 4 digit number for you. Let's play a bulls and co
 print(line)
 print("Enter a number:")
 print(line)
+
 while (user_number:= input(">>>")) != random_number:
   if not user_number.isdigit():
     print("Your input insn't number, please inser number again")
@@ -49,12 +58,7 @@ while (user_number:= input(">>>")) != random_number:
     print("The number must not start with zero, plese insert number again:")
     print(line)
     continue
-  digits = ["1","2","3","4","5","6","7","8","9"]
-  duplication = False
-  for cycle_number in range(0,3):
-    if user_number[cycle_number] in user_number[cycle_number+1]:
-      duplication = True
-      break
+  duplication = check_duplication(user_number)
   if duplication == True:
     print("The number contains duplication, plese insert number again:")
     continue
